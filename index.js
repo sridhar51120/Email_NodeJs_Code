74f37d1444ff44a68f27044747aa174cdff0704b
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-06-21 21:47:45 +0530

    google oauth

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const passport = require('passport');
-const fs = require('fs');
-
-const filePath = 'E:/AppConfig.json';
-fs.readFile(filePath, 'utf8', (err, data) => {
-    try {
-      const jsonData = JSON.parse(data);
-      const Google_site_api_keys = jsonData.Google_site_api_keys;
-      console.log('Data',Google_site_api_keys );
-
-    } catch (err) {
-      console.error('Error parsing JSON:', err);
-    }
-  });
-
-const GoogleSttrategy = require('passport-google-oauth20').Strategy;
-passport.use(new GoogleSttrategy());
+const express = require("express");
+const passport = require("passport");
+const keys = require("./config/keys");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    //google -> string is a one of the identifiers in statergy of "GoogleStrategy"
+    scope: ["profile", "email"], //Accessing Scopes for google to Administratater
+  })
+);
 
-// const PORT = process.env.PORT || 5000
-// app.listen(5000);
-
+app.get("/auth/google/callback", passport.authenticate("google"));
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
