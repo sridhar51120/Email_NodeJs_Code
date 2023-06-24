de2a027649a95ab71d1b87380fc581760a58d506
Sridhar <sridhardscv@gmail.com>
2023-06-24 12:27:44 +0530

    session initialize

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const coolieSession = require("cookie-session");
+const keys = require("./config/keys");
+const passport = require("passport");
+require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+//Cookies
+
+app.use(
+  coolieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // Cookie Age
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 // const PORT = process.env.PORT || 5000;
 app.listen(5000);
