6df260ccc00dd7e0d2b754f2effad06da6c4187e
Raahul <sridhardscv@gmail.com>
2023-06-29 18:47:11 +0530

    default authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,14 @@
-function authReducer(state = {}, action) {
-  console.log(action);
-  switch (action.type) {
+import {FETCH_USER} from '../actions/types';
+
+export default function(state=null,action){
+  // console.log(action);
+  switch(action.type){
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
 }
 
-export default authReducer;
+
+
