08d798f3f6c8437fbd3f43eb856c4c52973bbbe7
Raahul <sridhardscv@gmail.com>
2023-07-06 14:12:00 +0530

    login redirect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,39 @@
 import React from "react";
-import {Component} from "react";
-
+import { Component } from "react";
+import { connect } from "react-redux";
 class Header extends Component {
- render(){
-  return (
-    <div>
-      <nav>
-        <div className="nav-wrapper">
-          <a  href="" className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a href="">Login with Google</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
- }
+  // console.log(this.props);
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="" className="left brand-logo">
+              Emaily
+            </a>
+            <ul className="right">
+              { this. renderContent() }
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return {
+    auth: auth,
+  };
+}
+export default connect(mapStateToProps)(Header);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -22,7 +22,9 @@ module.exports = (app) => {
     passport.authenticate("github", { scope: ["user:email"] })
   );
 
-  app.get("/auth/github/callback", passport.authenticate("github"));
+  app.get("/auth/github/callback", passport.authenticate("github"),(req,res)=>{
+    res.redirect("/surveys");
+  });
 
   app.get("/api/logout", (req, res) => {
     req.logout();
