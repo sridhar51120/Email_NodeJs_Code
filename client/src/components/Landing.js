048cfd1862eeccd4d72db7e78fa4bf74cac28994
Raahul <sridhardscv@gmail.com>
2023-07-13 23:35:27 +0530

    initial commit

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Logo from '../landing_icon.jpg';
 
 function Landing() {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Emaily!</h1>
       Collect Feedback form your Users
+      <div>
+        <Logo />
+      </div>
     </div>
+
+
   );
 }
 
